b50aadda276d1751ec65418dad2a34f4169caf63
Philipp Mössl <pmoessl@outlook.de>
2024-04-21 18:05:28 +0200

    add form validation with error msgs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,16 +6,17 @@ import { BackendService } from '../service/backend.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user.class';
 import { ThemePalette } from '@angular/material/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements AfterViewInit , OnInit {
+export class LoginComponent implements AfterViewInit, OnInit {
   //Form Elements
   @ViewChild('loginForm') loginForm: ElementRef;
-  @ViewChild('registerForm') registerFrom: ElementRef;
+  @ViewChild('registerForm') registerForm: ElementRef;
   @ViewChild('resetForm') resetForm: ElementRef;
   //login Elements
   @ViewChild('loginEmail') loginEmail: ElementRef;
@@ -34,7 +35,7 @@ export class LoginComponent implements AfterViewInit , OnInit {
   showResetPassword = false;
   loginInProgress = false;
   registerInProgress = false;
-  resetInProgress = false ;
+  resetInProgress = false;
   durationInSeconds = 5;
 
   color: ThemePalette = 'primary';
@@ -44,6 +45,7 @@ export class LoginComponent implements AfterViewInit , OnInit {
     private sharedService: SharedService,
     private backendService: BackendService,
     public router: Router,
+    private formbuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
@@ -53,10 +55,10 @@ export class LoginComponent implements AfterViewInit , OnInit {
   ngAfterViewInit(): void {
     this.loginForm.nativeElement.addEventListener('submit', (event) => {
       event.preventDefault();
-      this.login();    
+      this.login();
     });
 
-    this.registerFrom.nativeElement.addEventListener('submit', (event) => {
+    this.registerForm.nativeElement.addEventListener('submit', (event) => {
       event.preventDefault();
       this.register();
     });
@@ -66,7 +68,7 @@ export class LoginComponent implements AfterViewInit , OnInit {
       this.resetPassword();
     });
 
-    
+
   }
 
   /**
@@ -99,31 +101,31 @@ export class LoginComponent implements AfterViewInit , OnInit {
    * 
    */
   async login() {
+    if (this.loginValidation()) {
+      //save remember me checkbox in shared service
+      this.sharedService.rememberMeActiv = this.rememberMe;
 
-    //save remember me checkbox in shared service
-    this.sharedService.rememberMeActiv = this.rememberMe;
-
-    if (this.rememberMe) {
-      localStorage.setItem('username', this.loginEmail.nativeElement.value);
-      localStorage.setItem('userpassword', this.loginPassword.nativeElement.value);
-    }
-
-    this.loginInProgress = true;
-    try {
-      let resp: any = await this.backendService.loginWithUsernameAndPassword(this.loginEmail.nativeElement.value, this.loginPassword.nativeElement.value);
-      if (resp['success'] == false) {
-        this.openSnackBar('Login failed');
-      } else {
-        localStorage.setItem('token', resp['token']);
-        localStorage.setItem('user', resp['username']);
-        this.sharedService.currentUser = resp['username'];
-        //router navigate
-        this.router.navigateByUrl('start-screen');
+      if (this.rememberMe) {
+        localStorage.setItem('username', this.loginEmail.nativeElement.value);
+        localStorage.setItem('userpassword', this.loginPassword.nativeElement.value);
       }
-    } catch (err) {
-      this.openSnackBar(err);
-    }
 
+      this.loginInProgress = true;
+      try {
+        let resp: any = await this.backendService.loginWithUsernameAndPassword(this.loginEmail.nativeElement.value, this.loginPassword.nativeElement.value);
+        if (resp['success'] == false) {
+          this.openSnackBar('Login failed');
+        } else {
+          localStorage.setItem('token', resp['token']);
+          localStorage.setItem('user', resp['username']);
+          this.sharedService.currentUser = resp['username'];
+          //router navigate
+          this.router.navigateByUrl('start-screen');
+        }
+      } catch (err) {
+        this.openSnackBar(err);
+      }
+    }
     this.loginInProgress = false;
   }
 
@@ -156,30 +158,33 @@ export class LoginComponent implements AfterViewInit , OnInit {
    * 
    */
   async register() {
-    if (this.checkConfirmPassword()) {
-      this.registerInProgress = true;
-      let user = new User({ 'username': this.registerUsername.nativeElement.value, 'email': this.registerEmail.nativeElement.value, 'password': this.registerPassword.nativeElement.value });
+    if (this.registerValidation()) {
+      if (this.checkConfirmPassword()) {
+        this.registerInProgress = true;
+        let user = new User({ 'username': this.registerUsername.nativeElement.value, 'email': this.registerEmail.nativeElement.value, 'password': this.registerPassword.nativeElement.value });
 
-      let resp = await this.backendService.registerNewUser(user);
+        let resp = await this.backendService.registerNewUser(user);
 
-      if (resp['success'] == true) {
-        this.openSnackBar('Registration success, please confirm email');
-        this.clearRegisterForm();
-      } else if (resp['success'] == false)
-        this.openSnackBar('Registration failed');
+        if (resp['success'] == true) {
+          this.openSnackBar('Registration success, please confirm email');
+          this.clearRegisterForm();
+        } else if (resp['success'] == false)
+          this.openSnackBar('Registration failed');
+      }
     }
-    this.registerInProgress = false ;
+
+    this.registerInProgress = false;
   }
 
   /**
    * clear input fields of register form
    * 
    */
-  clearRegisterForm(){
-    this.registerUsername.nativeElement.value = '' ;
-    this.registerEmail.nativeElement.value = '' ;
-    this.registerPassword.nativeElement.value = '' ;
-    this.registerConfirmPassword.nativeElement.value = '' ;
+  clearRegisterForm() {
+    this.registerUsername.nativeElement.value = '';
+    this.registerEmail.nativeElement.value = '';
+    this.registerPassword.nativeElement.value = '';
+    this.registerConfirmPassword.nativeElement.value = '';
   }
 
   /**
@@ -200,19 +205,79 @@ export class LoginComponent implements AfterViewInit , OnInit {
    * send email and generate a link to reset your password
    * 
    */
-  async resetPassword(){
-    this.resetInProgress = true ;
-    let userEmail = this.emailResetPassword.nativeElement.value ;
+  async resetPassword() {
+    if (this.resetPasswordValidation()) {
+      this.resetInProgress = true;
+      let userEmail = this.emailResetPassword.nativeElement.value;
 
-    let resp = await this.backendService.sendResetUserPasswordEmail(userEmail) ;
+      let resp = await this.backendService.sendResetUserPasswordEmail(userEmail);
 
-    if (resp['success'] == true) {
-      this.openSnackBar('Confirm the link in your email')
-    } else {
-      this.openSnackBar('Something went wrong :(')
+      if (resp['success'] == true) {
+        this.openSnackBar('Confirm the link in your email')
+      } else {
+        this.openSnackBar('Something went wrong :(')
+      }
+    }
+    this.resetInProgress = false;
+  }
+
+  /**
+   * validate the input fields for login form 
+   * 
+   * @returns if there is a validation error or not
+   */
+  loginValidation() {
+    // check username not empty
+    if (this.loginEmail.nativeElement.value === '') {
+      this.openSnackBar('Username is empty!');
+      return false;
     }
 
-    this.resetInProgress = false ;
+    // check password not empty
+    if (this.loginPassword.nativeElement.value === '') {
+      this.openSnackBar('Password is empty!');
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * validate the input fields for register form 
+   * 
+   * @returns if there is a validation error or not
+   */
+  registerValidation() {
+    // check email
+    if (!this.registerEmail.nativeElement.value.includes('@') || this.registerEmail.nativeElement.value.length < 4) {
+      this.openSnackBar('No correct email');
+      return false;
+    }
+
+    // check username
+    if (this.registerUsername.nativeElement.value.length < 3) {
+      this.openSnackBar('Username must be at least 3 characters long');
+      return false;
+    }
+
+    // check password
+    if (this.registerPassword.nativeElement.value.length < 5) {
+      this.openSnackBar('Password must be at least 5 characters long');
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * validate the input fields for reset password form 
+   * 
+   * @returns if there is a validation error or not
+   */
+  resetPasswordValidation() {
+    if (!this.emailResetPassword.nativeElement.value.includes('@') || this.emailResetPassword.nativeElement.value.length < 4) {
+      this.openSnackBar('No correct email');
+      return false;
+    }
+    return true;
   }
 
 }
